5fc6d0e37f622428b477b7c06a933097bab5d856
James Barber <james@jamesbarber.tech>
2023-06-26 14:14:18 +0100

    Added Achievement files

---
+++ app/Models/Achievement.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use User;
+
+class Achievement extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+
+    ];
+}
